7066d691dd8098b1a45dc6577b68d887bb82a28d
Miha Stele <stele1000@gmail.com>
2024-01-20 22:12:49 +0000

    Check metadata with stat

--- 9files.c
+++ 9files.c
@@ -42,9 +42,13 @@ int main(int argc, char *argv[])
 
     // file input
 
+    struct stat dbStat = {0};
+
     struct database_header_t head = {0};
 
-    int fd2 = open("./my-db.db", O_RDWR | O_CREAT, 0644);
+    // int fd2 = open("./my-db.db", O_RDWR | O_CREAT, 0644);
+
+    int fd2 = open("./malicious-db.db", O_RDWR | O_CREAT, 0644);
     if (fd2 == -1)
     {
         perror("Error opening file");
@@ -60,6 +64,23 @@ int main(int argc, char *argv[])
     printf("Version: %d\n", head.version);
     printf("Employees: %d\n", head.employees);
     printf("Filesize: %d\n", head.filesize);
+
+    if (fstat(fd2, &dbStat) < 0)
+    {
+        perror("Error getting file stats");
+        return -1;
+    }
+
+    printf("DB File size reported by stat: %ld\n", dbStat.st_size);
+
+    if (dbStat.st_size != head.filesize)
+    {
+        printf("GET OUTTA HERE HACKER!\n");
+        close(fd2);
+        return -2;
+    }
+
+    close(fd2);
 
     return 0;
 }
