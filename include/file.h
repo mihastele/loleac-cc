4190fe3e1deceabb87461e808fce303aa452cdff
Miha Stele <stele1000@gmail.com>
2024-01-22 12:36:17 +0000

    Create DB

--- include/file.h
+++ include/file.h
@@ -2,5 +2,7 @@
 #define FILE_H
 
 int open_file_rw(char *filename);
+int create_db_file(char *filename);
+int open_db_file(char *filename);
 
 #endif // FILE_H
--- src/main.c
+++ src/main.c
@@ -6,12 +6,19 @@
 #include "file.h"
 #include "parse.h"
 
+void print_usage()
+{
+    printf("Usage: ./db -f <filepath> [-n] <<to create a new DB>> \n");
+}
+
 int main(int argc, char *argv[])
 {
 
     char *filepath = NULL;
     bool newfile = false;
     int c;
+
+    int dbfd = -1; // database file descriptor
 
     while ((c = getopt(argc, argv, "nf:")) != -1)
     {
@@ -28,6 +35,32 @@ int main(int argc, char *argv[])
             printf("Unknown option: %c\n", c);
             break;
         default:
+            return -1;
+        }
+    }
+
+    if (filepath == NULL)
+    {
+        printf("No filepath specified\n");
+        print_usage();
+        return -1;
+    }
+
+    if (newfile)
+    {
+        dbfd = create_db_file(filepath);
+        if (dbfd == STATUS_ERROR)
+        {
+            printf("Unable to create DB file\n");
+            return -1;
+        }
+    }
+    else
+    {
+        dbfd = open_db_file(filepath);
+        if (dbfd == STATUS_ERROR)
+        {
+            printf("Unable to open DB file\n");
             return -1;
         }
     }
